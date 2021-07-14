797cd4de3c07b4912983e1375e5db58aa5d92ef4
suhas-malhotra <xyzcyz00@gmail.com>
2021-07-14 22:12:32 +0530

    basic pages setup added

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,11 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "./pages/home/App";
+import CallForPapers from "./pages/callForPapers/App";
+import ImportantDates from "./pages/importantDates/App";
+import Contact from "./pages/contact/App";
+import Speakers from "./pages/speakers/App";
 import Gallery from "./pages/gallery/App";
-
 
 function App() {
   return (
@@ -11,7 +14,19 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route path="/gallery">
-            <Gallery/>
+            <Gallery />
+          </Route>
+          <Route path="/call-for-papers">
+            <CallForPapers />
+          </Route>
+          <Route path="/speakers">
+            <Speakers />
+          </Route>
+          <Route path="/important-dates">
+            <ImportantDates />
+          </Route>
+          <Route path="/contact-us">
+            <Contact />
           </Route>
           <Route path="/">
             <Home />
---
+++ client/src/pages/speakers/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Navbar from "../../components/layouts/Navbar";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      Speakers
+    </div>
+  );
+};
+
+export default App;
