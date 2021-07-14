e08d73d540a2643a04361c0b6096e0cc2d90cebd
Amandeep <amandeepsingh8439@gmail.com>
2021-07-14 21:21:19 +0530

    Moving Header

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,15 @@ import Navbar from "./components/layouts/Navbar";
 import AboutUs from "./components/layouts/AboutUs";
 import Footer from "./components/layouts/Footer";
 import Home from "./components/layouts/Home"
+import MovingHeader from "./components/layouts/MovingHeader";
 function App() {
+  
   return (
     <div>
       <Navbar />
+      <MovingHeader/>
       <div className="sections">
+       
         <Home />
       </div>
       <AboutUs />
---
+++ client/src/components/layouts/MovingHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../../assets/css/MovingHeader.css";
+export const MovingHeader = () => {
+    return (
+        <div>
+       <marquee bgcolor="#90EE90" scrollamount="10" vspace="20" hspace="20" direction="right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime recusandae atque eligendi aut quo! Exercitationem.</marquee>
+            
+        </div>
+    )
+}
+export default MovingHeader;
