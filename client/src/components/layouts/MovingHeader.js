5bb7958a3785b614096c5c16bff35b75709b28ec
suhas-malhotra <xyzcyz00@gmail.com>
2021-07-15 16:22:13 +0530

    improved moving header

--- client/src/components/layouts/MovingHeader.js
+++ client/src/components/layouts/MovingHeader.js
@@ -1,11 +1,14 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/no-distracting-elements */
+import React from "react";
 import "../../assets/css/MovingHeader.css";
+
 export const MovingHeader = () => {
-    return (
-        <div>
-       <marquee bgcolor="#90EE90" scrollamount="10" vspace="20" hspace="20" direction="right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime recusandae atque eligendi aut quo! Exercitationem.</marquee>
-            
-        </div>
-    )
-}
+  return (
+    <div>
+      <marquee bgcolor="#90EE90" scrollamount="10" vspace="5" direction="left">
+        Registration is Open till 31st December 2021
+      </marquee>
+    </div>
+  );
+};
 export default MovingHeader;
