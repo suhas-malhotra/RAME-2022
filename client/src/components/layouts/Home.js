a97ae7b3e9a9dd74ed6d4d30b7935cc03522b418
Amandeep <amandeepsingh8439@gmail.com>
2021-07-17 18:16:07 +0530

    Home Page Improved

--- client/src/components/layouts/Home.js
+++ client/src/components/layouts/Home.js
@@ -20,25 +20,17 @@ export default function Intro() {
 
   return (
     <div className="intro" id="intro">
-      <div className="left">
-        <div className="imgContainer">
-          <img src={naruto} alt="" className="img-fluid p-4" />
-        </div>
-      </div>
-      <div className="right">
         <div className="wrapper">
           <h1>
             3<sup>rd</sup>
-          </h1>
-          <h1>International Conference</h1>
-          <h1>on</h1>
-          <h1>Recent Advances in Mechanical Engineering (RAME)</h1>
-
+         <h1> International Conference</h1>
+        <h1>  on </h1>
+            Recent Advances in Mechanical Engineering </h1>
+            <h1>(RAME-2022)</h1>
           <h2>8 July - 9 july'22</h2>
-          <h3 className="text-center">
+          <h2 className="text-center">
             <span ref={textRef}></span>
-          </h3>
-        </div>
+          </h2>
       </div>
     </div>
   );
