9634f50eaa8e12a1879099f428b31cad8c55b57a
Amandeep <amandeepsingh8439@gmail.com>
2021-07-18 16:40:08 +0530

    Imp Dates done

--- client/src/pages/importantDates/App.js
+++ client/src/pages/importantDates/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 
 import Navbar from "../../components/layouts/Navbar";
-
+import ImpDates from "../../components/layouts/ImpDates";
+import MovingHeader from "../../components/layouts/MovingHeader";
 import "./App.css";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      Important Dates
+      <MovingHeader/>
+     <ImpDates/>
     </div>
   );
 };
