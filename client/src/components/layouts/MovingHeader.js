deb384cd1daaf63b97a56426b0084f5d80641bcf
suhas-malhotra <xyzcyz00@gmail.com>
2022-04-09 22:46:12 +0530

    .

--- client/src/components/layouts/MovingHeader.js
+++ client/src/components/layouts/MovingHeader.js
@@ -6,7 +6,7 @@ export const MovingHeader = () => {
   return (
     <div>
       <marquee bgcolor="#FFC000" scrollamount="10" vspace="5" direction="left">
-        Deadline for Paper submission is 31<sup>st</sup> March' 2022
+        Deadline for Paper submission is 30<sup>th</sup> April' 2022
       </marquee>
     </div>
   );
