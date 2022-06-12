3774c6aeba346f9f2c7e73145e4a709f36f9ef82
suhas-malhotra <xyzcyz00@gmail.com>
2022-06-12 18:54:20 +0530

    .

--- client/src/components/layouts/MovingHeader.js
+++ client/src/components/layouts/MovingHeader.js
@@ -6,7 +6,10 @@ export const MovingHeader = () => {
   return (
     <div>
       <marquee bgcolor="#FFC000" scrollamount="10" vspace="5" direction="left">
-        Deadline for final round of call for papers is extended to June 6, 2022
+        Deadline for final round of call for papers is extended to June 6, 2022{" "}
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSed9JUz7LGVKSMMlazEh7qLlO649Pve9K20EUhnBUpUimKHjQ/viewform">
+          Click here to Register
+        </a>
       </marquee>
     </div>
   );
