641fba4a57116c800b9a4b32c852f4bdf4c9dcc2
suhas-malhotra <xyzcyz00@gmail.com>
2022-06-25 10:31:19 +0530

    .

--- client/src/components/layouts/MovingHeader.js
+++ client/src/components/layouts/MovingHeader.js
@@ -1,14 +1,24 @@
 /* eslint-disable jsx-a11y/no-distracting-elements */
 import React from "react";
 import "../../assets/css/MovingHeader.css";
-
+import guidelines from "../../assets/docs/guidelines.pptx";
+import papers from "../../assets/docs/papers.pdf";
 export const MovingHeader = () => {
   return (
     <div>
       <marquee bgcolor="#FFC000" scrollamount="10" vspace="5" direction="left">
-        Deadline for final round of call for papers is extended to June 6, 2022{" "}
         <a href="https://docs.google.com/forms/d/e/1FAIpQLSed9JUz7LGVKSMMlazEh7qLlO649Pve9K20EUhnBUpUimKHjQ/viewform">
           Click here to Register
+        </a>{" "}
+        ||{" "}
+        <a href={guidelines} download>
+          {" "}
+          Guidelines
+        </a>{" "}
+        ||{" "}
+        <a href={papers} download>
+          {" "}
+          Tentative List of Selected Papers
         </a>
       </marquee>
     </div>
